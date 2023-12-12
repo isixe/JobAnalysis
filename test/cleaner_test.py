a1ea60b162c1539c5fafd806013c6ad297742ea0
isixe <isixe@outlook.com>
2023-12-12 15:20:15 +0800

    :white_check_mark: test(test): add cleaner test example

---
+++ test/cleaner_test.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+# @Time    : 2023/12/12 15:15
+# @Author  : Exisi
+# @Version : python3.10.6
+# @Desc    : data cleaner test
+
+from clean import jobcleaner51
+
+if __name__ == '__main__':
+    jobcleaner51.start(save_engine='both')
